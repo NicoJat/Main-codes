53af60664f750650f586f627173f453b444dd9b7
Nicoja <nicolas-jativa12@gmail.com>
2023-10-01 23:52:40 -0500

    01-10-2023

--- Diseño_Sismoresistente/BNewmark_DS_GR2_2023_02.py
+++ Diseño_Sismoresistente/BNewmark_DS_GR2_2023_02.py
@@ -1,36 +1,54 @@
 import numpy as np
 
+def BNewmarkJr(Sg2,zi,T,m,w):
 
-class BNewmark_DS_GR2_2023_02:
+    delta = 0.01
+    Kp = ((2*np.pi/T)**2)*m
 
-    def __init__(self, Sg2,zi,T,m,w):
-        self.T = T
-        self.m = m
-        self.w = w
-        self.zi = zi
-        self.Sg2 = Sg2
+    xn1 = np.zeros(len(Sg2))
+    xvn1 = np.zeros(len(Sg2))
+    xan1 = np.zeros(len(Sg2))
+
+    xo = 0
+    xvo = 0
+    xn1[0] = xo
+    xvn1[0] = xvo
+    xan1[0] = ((-Sg2[0,1]*m) - (2*zi*w*m*xvo) - ((w**2)*xo))/m
+
+    for i in np.arange(1, len(Sg2)):
+        xn1[i] = xn1[i-1] + (delta*xvn1[i-1]) + (((delta**2)/2)*xan1[i-1])
+        xvn1[i] = xvn1[i-1] + (delta*(0.5*(xan1[i-1] + xan1[i])))
+        xan1[i] = (1/(m+(2*zi*w*delta*0.5*m)))*((-Sg2[i,1]*m) - (Kp*xn1[i]) - ((2*zi*w*m)*(xvn1[i-1]+(delta*0.5*xan1[i-1]))))
+
+    Sa_max = np.max(np.abs(xan1))
+
+    return xn1, xvn1, xan1, Sa_max
+
+'''
+def BNewmarkClassic():
+'''
+
+def Spec_NEC(n,z,fa,fd,fs,r,I,R):
+    To = 0.1*fs*(fd/fa)
+    Tc = 0.55*fs*(fd/fa)
+    Tl = 2.4*fd
+
+    j = 0
+    Spec = []
+    Tmp = []
+    for T in np.arange(0,Tl,0.01):
+        if T <= To:
+            Tmp.append(T)
+            Spec.append(z*fa*(1 + ((n-1)*(T/To))))
+        elif T > To and T < Tc:
+            Tmp.append(T)
+            Spec.append(n*z*fa)
+        else:
+            Tmp.append(T)
+            Spec.append(n*z*fa*((Tc/T)**r))
+        
+        j+=1
+
+    return Spec, Tmp
 
 
-    def BNewmark(self):
-
-        delta = 0.01
-        Kp = ((2*np.pi/self.T)**2)*self.m
-
-        xn1 = np.zeros(len(self.Sg2))
-        xvn1 = np.zeros(len(self.Sg2))
-        xan1 = np.zeros(len(self.Sg2))
-
-        xo = 0
-        xvo = 0
-        xn1[0] = xo
-        xvn1[0] = xvo
-        xan1[0] = ((-self.Sg2[0,1]*self.m) - (2*self.zi*self.w*self.m*xvo) - ((self.w**2)*xo))/self.m
-
-        for i in np.arange(1, len(self.Sg2)):
-            xn1[i] = xn1[i-1] + (delta*xvn1[i-1]) + (((delta**2)/2)*xan1[i-1])
-            xvn1[i] = xvn1[i-1] + (delta*(0.5*(xan1[i-1] + xan1[i])))
-            xan1[i] = (1/(self.m+(2*self.zi*self.w*delta*0.5*self.m)))*((-self.Sg2[i,1]*self.m) - (Kp*xn1[i]) - ((2*self.zi*self.w*self.m)*(xvn1[i-1]+(delta*0.5*xan1[i-1]))))
-
-        Sa_max = np.max(np.abs(xan1))
-
-        return xn1, xvn1, xan1, Sa_max
