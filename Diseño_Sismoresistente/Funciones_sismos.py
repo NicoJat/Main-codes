f3243214322f13066b7b5d0060b0842169854e36
Nicoja <nicolas-jativa12@gmail.com>
2023-10-21 20:28:32 -0500

    21-10-2023

--- Diseño_Sismoresistente/Funciones_sismos.py
+++ Diseño_Sismoresistente/Funciones_sismos.py
@@ -28,27 +28,55 @@ def BNewmarkJr(Sg2,zi,T,m,w):
 def BNewmarkClassic():
 '''
 
-def Spec_NEC(n,z,fa,fd,fs,r,I,R):
+def Spec_NEC(n, z, fa, fd, fs, r, I, R, fip, fie):
     To = 0.1*fs*(fd/fa)
     Tc = 0.55*fs*(fd/fa)
     Tl = 2.4*fd
 
     j = 0
     Spec = []
+    SpecI = []
     Tmp = []
     for T in np.arange(0,Tl,0.01):
         if T <= To:
             Tmp.append(T)
             Spec.append(z*fa*(1 + ((n-1)*(T/To))))
+            SpecI.append(n*z*fa/(R*fip*fie))
         elif T > To and T < Tc:
             Tmp.append(T)
             Spec.append(n*z*fa)
+            SpecI.append(n*z*fa/(R*fip*fie))
         else:
             Tmp.append(T)
             Spec.append(n*z*fa*((Tc/T)**r))
+            SpecI.append(n*z*fa*((Tc/T)**r)/(R*fip*fie))
         
         j+=1
 
-    return Spec, Tmp
+    return Spec, SpecI, Tmp
 
 
+'''
+Matriz de Rigidez y masas
+'''
+
+def K_M(num_col, H, E, tipo, I, vector, m, coef_castigo):
+
+    k = []
+    M = np.zeros((len(H), len(H)))
+    for i in range(len(H)):
+        ki = ((num_col*12)*E*I[i])/(H[i]**3)
+        k.append(ki)
+        M[i,i] = m[i]
+    
+    j = 0
+    K = np.zeros((len(H), len(H)))
+    for i in range(len(k)-1):
+        K[i,i] = k[i] + k[i+1]
+        K[i,j+1] = -k[i+1]
+        K[i+1,j] = -k[i+1]
+        K[len(k)-1,len(k)-1] = k[len(H)-1]
+        j+=1
+
+    return K, M
+    
