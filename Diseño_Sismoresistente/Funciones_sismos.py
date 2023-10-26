dc68404b2738ee8d9a022bc14f578348e1fc5f73
Nicoja <nicolas-jativa12@gmail.com>
2023-10-25 21:57:40 -0500

    25-10-2023

--- Diseño_Sismoresistente/Funciones_sismos.py
+++ Diseño_Sismoresistente/Funciones_sismos.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+'''
+def BNewmarkClassic():
+'''
+
 def BNewmarkJr(Sg2,zi,T,m,w):
 
     delta = 0.01
@@ -24,10 +28,10 @@ def BNewmarkJr(Sg2,zi,T,m,w):
 
     return xn1, xvn1, xan1, Sa_max
 
-'''
-def BNewmarkClassic():
-'''
 
+'''
+Espesctro Respuesta de la NEC
+'''
 def Spec_NEC(n, z, fa, fd, fs, r, I, R, fip, fie):
     To = 0.1*fs*(fd/fa)
     Tc = 0.55*fs*(fd/fa)
@@ -57,6 +61,58 @@ def Spec_NEC(n, z, fa, fd, fs, r, I, R, fip, fie):
 
 
 '''
+Espesctro Respuesta de la ASCE 7-16
+'''
+def Spec_ASCE7(Tl, Fa, Fv, Ss, S1, limite):
+    Sms = Fa*Ss
+    Sm1 = Fv*S1
+    Sds = (2/3)*Sms
+    Sd1 = (2/3)*Sm1
+    Ts = Sd1/Sds
+    To = 0.2*(Sd1/Sds)
+
+    '''
+    1era_porción de la curva, 0 <= T < T0
+    Ver en sección 11.4-6
+    Sa = Sds*(0.4+(0.6*T/T0))
+
+    2da_porción de la curva, T0 <= T <= Ts
+    Sds
+
+    3era_porción de la curva, Ts <= T <= Tl
+    Sa = Sd1/T
+
+    4ta_porción de la curva
+    Sa = (Sd1*Tl)/(T**2)
+    '''
+
+    j = 0
+    Spec_ASCE = []
+    #SpecI = []
+    Tmp = []
+    for T in np.arange(0,limite,0.01):
+        if T <= To:
+            Tmp.append(T)
+            Spec_ASCE.append(Sds*(0.4+(0.6*T/To)))
+            #SpecI.append(n*z*fa/(R*fip*fie))
+        elif T > To and T < Ts:
+            Tmp.append(T)
+            Spec_ASCE.append(Sds)
+            #SpecI.append(n*z*fa/(R*fip*fie))
+        elif T > Ts and T < Tl:
+            Tmp.append(T)
+            Spec_ASCE.append(Sd1/T)
+            #SpecI.append(n*z*fa*((Ts/T)**r)/(R*fip*fie))
+        else:
+            Tmp.append(T)        
+            Spec_ASCE.append((Sd1*Tl)/(T**2))    
+        j+=1
+
+    return Spec_ASCE, Tmp
+
+
+
+'''
 Matriz de Rigidez y masas
 '''
 
@@ -79,4 +135,27 @@ def K_M(num_col, H, E, tipo, I, vector, m, coef_castigo):
         j+=1
 
     return K, M, k
+
+'''
+Matriz de Rigidez y masas
+'''
+
+def K_M(num_col, H, E, tipo, I, vector, m, coef_castigo):
+
+    k = []
+    M = np.zeros((len(H), len(H)))
+    for i in range(len(H)):
+        ki = ((num_col*12)*E*I[i])/(H[i]**3)
+        k.append(ki)
+        M[i,i] = m[i]
     
+    j = 0
+    K = np.zeros((len(H), len(H)))
+    for i in range(len(k)-1):
+        K[i,i] = k[i] + k[i+1]
+        K[i,j+1] = -k[i+1]
+        K[i+1,j] = -k[i+1]
+        K[len(k)-1,len(k)-1] = k[len(H)-1]
+        j+=1
+
+    return K, M, k
